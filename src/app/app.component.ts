9dbbdbf848893304196409d719735064c22cb516
Gerardo Soto <gerardosoto93@gmail.com>
2017-06-03 00:58:25 +0000

    Display Tests in History Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { LandingPage } from '../pages/landing/landing';
+// import { LandingPage } from '../pages/landing/landing';
+import { HistoryPage } from '../pages/history/history';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LandingPage;
+  rootPage:any = HistoryPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the HistoryPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { ResultsPage } from '../results/results';
+
+
 @IonicPage()
 @Component({
   selector: 'page-history',
   templateUrl: 'history.html',
 })
 export class HistoryPage {
+  
+  tests: any = []; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HistoryPage');
+    this.tests = JSON.parse(window.localStorage.getItem("tests")) || [];
+    console.log(this.tests);
+  }
+  
+  goToResult(test) {
+    this.navCtrl.push(ResultsPage, {
+      test: test
+    });
   }
 
 }
