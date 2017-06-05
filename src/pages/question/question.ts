66142492d60e927d77b92f630a7957aa8a288442
vin ho <vincenthoang00@gmail.com>
2017-06-05 15:07:38 -0700

    styles are being added to specific array, need to fix so values are pushed in instead

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -59,8 +59,207 @@ let apiQuestions = [
       "Keyed": false,
       "Text": "Am quiet around strangers.",
       "Style": "Extraversion"
-  }
-];
+  },
+  {
+      "Keyed": true,
+      "Text": "Am interested in people.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Sympathize with others' feelings.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Have a soft heart.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Take time out for others.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Feel others' emotions.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Make people feel at ease.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Am not really interested in others.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Insult people.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Am not interested in other people's problems.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Feel little concern for others.",
+      "Style": "Agreeableness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Am always prepared.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Pay attention to details.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Get chores done right away.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Like order.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Follow a schedule.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Am exacting in my work.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Leave my belongings around.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Make a mess of things.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Often forget to put things back in their proper place.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": false,
+      "Text": "Shirk my duties.",
+      "Style": "Conscientiousness"
+  },
+  {
+      "Keyed": true,
+      "Text": "Am relaxed most of the time.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": true,
+      "Text": "Seldom feel blue.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Get stressed out easily.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Worry about things.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Am easily disturbed.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Get upset easily..",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Change my mood a lot.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Have frequent mood swings.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Get irritated easily.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": false,
+      "Text": "Often feel blue.",
+      "Style": "Emotional Stability"
+  },
+  {
+      "Keyed": true,
+      "Text": "Have a rich vocabulary.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Have a vivid imagination.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Have excellent ideas.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Am quick to understand things.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Use difficult words.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Spend time reflecting on things.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": true,
+      "Text": "Am full of ideas.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": false,
+      "Text": "Have difficulty understanding abstract ideas.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": false,
+      "Text": "Am not interested in abstract ideas.",
+      "Style": "Intellect"
+  },
+  {
+      "Keyed": false,
+      "Text": "Do not have a good imagination.",
+      "Style": "Intellect"
+  }];
 
 @IonicPage()
 @Component({
@@ -71,6 +270,7 @@ let apiQuestions = [
 export class QuestionPage {
   @ViewChild(Slides) slides:Slides;
   questions: any = apiQuestions;
+  testAnswers: any = {};
 
   constructor(
     public navCtrl: NavController,
@@ -80,17 +280,27 @@ export class QuestionPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QuestionPage');
-    console.log(apiQuestions);
+    this.slides.lockSwipes(true);
+    this.testAnswers = {
+      "Extraversion": [],
+      "Agreeableness": [],
+      "Conscientiousness": [],
+      "Emotional Stability": []
+    };
   }
 
-  slideNext() {
+  nextSlide(option) {
     console.log("Submitted");
+    console.log(option.Style);
+    this.testAnswers[option.Style].push(option.Style);
     if(this.slides.getActiveIndex() + 1 !== apiQuestions.length) {
       this.slides.lockSwipes(false);
       this.slides.slideTo(this.slides.getActiveIndex() + 1);
       this.slides.lockSwipes(true);
+      console.log(this.testAnswers);
     } else {
-      
+      //Finished the test
+
     }
 
   }
